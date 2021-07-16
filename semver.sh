04e5b0887667853f2dddb1c1ea42b0d4a9558b28
Marco Lovazzano <marco.lovazzano@gmail.com>
2021-07-16 15:51:37 +0200

    Create semver.sh

---
+++ semver.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#--------------------------------------------------------------------------------------------------
+# Semantic Version
+# Copyright (c) Marco Lovazzano
+# Licensed under the GNU General Public License v3.0
+# http://github.com/martcus
+#--------------------------------------------------------------------------------------------------
+
+echo "Hello World!"
